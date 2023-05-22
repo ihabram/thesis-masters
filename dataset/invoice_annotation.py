882d8f3d54f72e65fdfba3c19fc320b1797a137c
ihabram <habramistvan@gmail.com>
2023-05-22 17:33:13 +0200

    Fill the invoice class

--- dataset/invoice_annotation.py
+++ dataset/invoice_annotation.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 class CleanInvoice():
     def __init__(self, image_loc, output_loc) -> None:
@@ -18,3 +18,35 @@ class CleanInvoice():
             for coord in labels[l]:
                 self.clear_entity(coord)
         self.image.save(self.output_loc)
+
+
+class FillInvoice():
+    def __init__(self, empty_image_loc, output_loc) -> None:
+        self.empty_image = empty_image_loc
+        self.output_loc = output_loc
+
+    def get_font_size(self, bounding_box):
+        # TODO: More sophisticated function which is universal for every invoice
+        height = bounding_box[1][1] - bounding_box[0][1]
+        if height > 25:
+            return 23
+        else:
+            return 18
+    
+    def fill_invoice(self, labels, fake_data, show, save, type):
+        for i, document in enumerate(fake_data):
+            invoice = Image.open(self.empty_image)
+            draw = ImageDraw.Draw(invoice)
+            # Iterate through the entities in a document
+            for entity in document:
+                # If the same label is presented on the document, iterate through the positions
+                for entity_instance in labels[entity]:
+                    
+                    font_size = self.get_font_size(entity_instance)
+                    font = ImageFont.truetype("arial.ttf", font_size)
+                    draw.text(tuple(entity_instance[0]), document[entity], fill='black', font=font)
+
+            if show == True:
+                invoice.show()
+            if save == True:
+                invoice.save(self.output_loc + type + '_' + str(i) + '.tif')
