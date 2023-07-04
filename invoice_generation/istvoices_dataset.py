00240e555fcf7bd2a1834509a1e3f8d125598d44
ihabram <habramistvan@gmail.com>
2023-07-04 08:46:48 +0200

    Named entities in order

--- invoice_generation/istvoices_dataset.py
+++ invoice_generation/istvoices_dataset.py
@@ -1,4 +1,5 @@
 '''
+This script turns the raw files into a dataset can be used universally.
 Template:  https://huggingface.co/docs/datasets/v1.4.0/add_dataset.html
 Reference: https://huggingface.co/datasets/nielsr/funsd-layoutlmv3/blob/main/funsd-layoutlmv3.py
 '''
@@ -66,14 +67,15 @@ class IstVoices(datasets.GeneratorBasedBuilder):
                     "bboxes": datasets.Sequence(datasets.Sequence(datasets.Value("int64"))),
                     "ner_tags": datasets.Sequence(
                         datasets.features.ClassLabel(
-                            names=['B-R_NAME',   'B-R_STREET', 'B-R_HOUSENUMBER', 'B-R_ZIP',    'B-R_CITY',     'B-R_COUNTRY', 'B-R_VAT',
-                                   'I-R_NAME',   'I-R_STREET', 'I-R_HOUSENUMBER', 'I-R_ZIP',    'I-R_CITY',     'I-R_COUNTRY', 'I-R_VAT',
-                                   'B-S_NAME',   'B-S_STREET', 'B-S_HOUSENUMBER', 'B-S_ZIP',    'B-S_CITY',     'B-S_COUNTRY', 'B-S_VAT',
-                                   'I-S_NAME',   'I-S_STREET', 'I-S_HOUSENUMBER', 'I-S_ZIP',    'I-S_CITY',     'I-S_COUNTRY', 'I-S_VAT',
-                                   'B-S_BANK',   'B-S_BIC',    'B-S_IBAN',        'B-S_TEL',    'B-S_EMAIL',
-                                   'I-S_BANK',   'I-S_BIC',    'I-S_IBAN',        'I-S_TEL',    'I-S_EMAIL',
-                                   'B-I_NUMBER', 'B-I_DATE',   'B-I_DUEDATE',     'B-I_AMOUNT', 'B-I_CURRENCY',
-                                   'I-I_NUMBER', 'I-I_DATE',   'I-I_DUEDATE',     'I-I_AMOUNT', 'I-I_CURRENCY', 'OTHER']
+                            names=['OTHER',
+                                   'B-R_NAME', 'I-R_NAME', 'B-R_STREET', 'I-R_STREET', 'B-R_HOUSENUMBER', 'I-R_HOUSENUMBER',
+                                   'B-R_ZIP',  'I-R_ZIP',  'B-R_CITY',   'I-R_CITY',   'B-R_COUNTRY', 'I-R_COUNTRY', 'B-R_VAT', 'I-R_VAT',
+                                   'B-S_NAME', 'I-S_NAME', 'B-S_STREET', 'I-S_STREET', 'B-S_HOUSENUMBER', 'I-S_HOUSENUMBER',
+                                   'B-S_ZIP',  'I-S_ZIP',  'B-S_CITY',   'I-S_CITY',   'B-S_COUNTRY', 'I-S_COUNTRY', 'B-S_VAT', 'I-S_VAT',          
+                                   'B-S_BANK', 'I-S_BANK',  'B-S_BIC', 'I-S_BIC', 'B-S_IBAN', 'I-S_IBAN', 'B-S_TEL', 'I-S_TEL', 
+                                   'B-S_EMAIL', 'I-S_EMAIL', 'B-I_NUMBER', 'I-I_NUMBER', 'B-I_DATE', 'I-I_DATE', 'B-I_DUEDATE',  'I-I_DUEDATE',
+                                   'B-I_AMOUNT', 'I-I_AMOUNT', 'B-I_CURRENCY', 'I-I_CURRENCY'
+                                ]
                         )
                     ),
                     "image": datasets.features.Image(),
